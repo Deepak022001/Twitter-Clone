450eaa3e9ccf5bae80ca1b5e72a41e2e9c9a262b
deepak022001 <studydipu@gmail.com>
2024-03-20 00:05:40 +0530

    Verifying and Generating JWT

---
+++ BackEnd/build/services/jwt.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const db_1 = require("../clients/db");
+class JWTService {
+}
+(generateTokenForUser) => ;
+(userId) => {
+    const user = yield db_1.PrismaClient.user.Unique;
+};
