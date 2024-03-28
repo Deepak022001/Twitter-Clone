11d691cdc137dc2ccb48c25b587ccf7f522f4b14
deepak022001 <studydipu@gmail.com>
2024-03-28 21:48:38 +0530

    Implemented Goggle Login Button

--- FrontEnd/pages/_app.tsx
+++ FrontEnd/pages/_app.tsx
@@ -2,11 +2,13 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Inter, Quicksand } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
-
+import { GoogleOAuthProvider } from '@react-oauth/google';
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className={inter.className}>
-      <Component {...pageProps} />;
-    </div>
+    <GoogleOAuthProvider clientId='795459890942-9ksajl0ko9gumkb7jhc9g47s25oltdmh.apps.googleusercontent.com'>
+      <div className={inter.className}>
+        <Component {...pageProps} />;
+      </div>
+    </GoogleOAuthProvider>
   );
 }
--- FrontEnd/pages/index.tsx
+++ FrontEnd/pages/index.tsx
@@ -2,6 +2,7 @@ import { BsBell, BsBookmark, BsEnvelope, BsTwitter } from 'react-icons/bs';
 import { SlOptions } from 'react-icons/sl';
 import { BiHash, BiHomeCircle, BiMoney, BiUser } from 'react-icons/bi';
 import FeedCard from './FeedCard';
+import { GoogleLogin } from '@react-oauth/google';
 
 interface TwitterSideBarIcons {
   title: string;
@@ -85,7 +86,19 @@ export default function Home() {
           <FeedCard />
           <FeedCard />
         </div>
-        <div className='col-span-3'></div>
+        <div className='col-span-3 p-5'>
+          <div className='border p-5 bg-slate-700'>
+            <h1>New to Twitter?</h1>
+            <GoogleLogin
+              onSuccess={(credentialResponse) => {
+                console.log(credentialResponse);
+              }}
+              onError={() => {
+                console.log('Login Failed');
+              }}
+            />
+          </div>
+        </div>
       </div>
       {/* main div finished here */}
     </div>
